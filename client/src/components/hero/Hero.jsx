bed42b84feefb77eea6a16fdaf3a4f1ddda5a3d9
khireddinbou <kheiereddin2004@gmail.com>
2023-06-08 19:50:50 +0100

    change hero img

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -5,14 +5,10 @@ const Hero = () => {
     <div className="hero">
       <span className="bg-text">MyCar.com</span>
       <div className="left">
-        
         <span>Search For Your Dream Car in a matter of seconds.</span>
       </div>
       <div className="right">
-        <img
-          src="https://www.tdmobiledetailing.com/_nuxt/img/hero-car-mobile.044e46d.png"
-          alt="car"
-        />
+        <img src="./hero-car.png" alt="car" />
       </div>
     </div>
   );
